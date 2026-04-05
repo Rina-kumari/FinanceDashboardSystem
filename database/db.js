94c476b26b2b6ad5ca3df841ce216ad658c4e54b
Rina Kumari <rinas37@gmail.com>
2026-04-05 12:09:24 +0400

    code changed

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export const dbconnection = () => {
     mongoose
-        .connect(process.env.MONGO_URL, {
+        .connect(process.env.MONGO_URI,{
             dbName: "FinancialRecordManagement",
         })
         .then(() => {
